df5ecc774560f6c7dd8ebeac6ab16b26bf9e0489
Protsiuk-Prits Dmytro <78557776+ProtsyukPrits@users.noreply.github.com>
2022-10-05 22:52:44 +0200

    created hm3-image-finger

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-export const App = () => {
-  return (
-    <div
+import { Component } from "react";
+import { addBaseFetch } from "./services/api";
+export class App extends Component {
+  
+    render() {
+      return (
+        <div
       style={{
         height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
+        display: 'block',
+        fontSize: 20,
         color: '#010101'
       }}
     >
-      React homework template
+      
     </div>
-  );
+      )
+    }
 };
